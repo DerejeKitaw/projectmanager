fa0fe7447881ac1998e2d70e5b7ad137d3b5c1fd
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-07-28 21:53:11 -0400

    data moved from constructor

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 import Projects from './component/Projects'
 class App extends Component {
-constructor(){
-  super();
-  this.state={
-    projects:[
-      {title: 'Business Website',
-      category: 'Web Design'},
-      {title: 'Social App',
-      category: 'Mobile Development'},
-      {title: 'Ecommerce Shoping Cart',
-      category: 'Web Development'}
-    ]
+  constructor() {
+    super();
+    this.state = {
+      projects: []
+    }
   }
-}
-
+  componentWillMount() {
+    this.setState(
+      {
+        projects: [
+          {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Ecommerce Shoping Cart',
+          category: 'Web Development'
+        }
+        ]
+      }
+    )
+  }
   render() {
     return (
       <div className="App">
-        <Projects projects={this.state.projects}/>
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
