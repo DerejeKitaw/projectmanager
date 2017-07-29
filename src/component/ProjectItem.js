678d16f92f69f4aa25ab9360dca65665a88f1763
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-07-29 00:12:07 -0400

    delete item implemented

--- src/component/ProjectItem.js
+++ src/component/ProjectItem.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 
 
 class ProjectItem extends Component {
+  deleteProject(id){
+    // console.log("works");
+    this.props.onDelete(id);
+  }
   render() {
       console.log(this.props);
     return (
       <li className="Project">
-        <strong>{this.props.project.id}</strong>  {this.props.project.category}
+        <strong>{this.props.project.id}</strong>  {this.props.project.category} <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>X</a>  
       </li>
     );
   }
