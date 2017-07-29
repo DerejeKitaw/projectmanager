9c4d1a185f31854bd5f3ca74f662c3efa1a97c89
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-07-28 22:11:37 -0400

    form title and category added in the form

--- src/component/AddProject.js
+++ src/component/AddProject.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 class AddProject extends Component {
+    // Set default category
+    static defaultProps = {
+        categories: [
+            'web Design' , 'Web Development', 'Mobile Development'
+        ]
+    }
   render() {
+      let categoryOptions = this.props.categories.map(category =>{
+          return <option key={category} value="category">{category}</option>
+      });
     return (
       <div>
           <h3>Add Project</h3>
+          <form action="">
+              <div>
+                  <lable>Title</lable>
+                  <input type="text" ref="title"/>
+              </div>
+              <div>
+                  <lable>Category</lable>
+                  <select ref="category">
+                    {categoryOptions}
+                  </select>
+              </div>
+          </form>
       </div>
     );
   }
